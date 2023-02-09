15acb4a771ca46bd4dfd9d7a907b991433a27758
Adam <AdamJPerry@Students.k12k.com>
2023-02-09 09:35:29 -0500

    ADDED THE GETTER TO THE GYRO.

--- src/main/java/frc/robot/commands/sensors/RomiGyro.java
+++ src/main/java/frc/robot/commands/sensors/RomiGyro.java
@@ -8,21 +8,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RomiGyro extends SubsystemBase {
     private SimDouble m_simAngleZ;
-
     private double m_angleZOffset;
+
+    private SimDouble m_simAngleX;
+
+    private SimDouble m_simAngleY;
 
     LinearFilter smoothFilter = LinearFilter.singlePoleIIR(0.1, 0.02);
 
-
+    /**
+     * creates values to be used in other functions.
+     *  sets our simAngleZ as a boolean to determine the input as T/F.
+     */
     private RomiGyro() {
         SimDevice gyroSimDevice = SimDevice.create("Gyro:RomiGyro"); 
 
         if(gyroSimDevice != null) {
             gyroSimDevice.createBoolean("init", Direction.kOutput, true);
             m_simAngleZ = gyroSimDevice.createDouble("angle_z", Direction.kOutput, 0.0);
+
+            m_simAngleX = gyroSimDevice.createDouble("angle x", Direction.kOutput, 0.0);
+
+            m_simAngleY = gyroSimDevice.createDouble(getName(), Direction.kOutput, 0.0);
         }
     }
 
+    /**
+     * calculates the angle based of the gyro
+     * 
+     * @return the value of angle Z
+     */
     public double getAngleZ() {
         if(m_simAngleZ != null) {
             return smoothFilter.calculate(m_simAngleZ.get()) - m_angleZOffset;
@@ -30,9 +45,34 @@ public class RomiGyro extends SubsystemBase {
         return 0.0;
     }
 
+    /**
+     * @return
+     */
+    public double getAngleX() {
+        if(m_simAngleZ != null) {
+            return smoothFilter.calculate(m_simAngleX.get()) - m_angleZOffset;
+        }
+        return 0.0;
+    }
+
+    /**
+     * @return
+     */
+    public double getAngleY() {
+        if(m_simAngleZ != null) {
+            return smoothFilter.calculate(m_simAngleY.get()) - m_angleZOffset;
+        }
+        return 0.0;
+    }
+
+    /**
+     * resets the angle until it's null 
+     */
     public void reset() {
         if(m_simAngleZ != null) {
             m_angleZOffset = m_simAngleZ.get();
+            m_angleZOffset = m_simAngleX.get();
+            m_angleZOffset = m_simAngleY.get();
         }
     }
 }
